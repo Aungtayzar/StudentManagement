ff532f049903c7ee0f5404dae5f1caf204a37b0d
tay zar <aungtay69@gmail.com>
2024-11-03 14:46:31 +0630

    first commit 3/11/2024

---
+++ resources/views/layouts/adminindex.blade.php
@@ -0,0 +1,62 @@
+@include('layouts.adminheader')
+{{-- start reactjs or vuejs  --}}
+<div id="app">
+
+
+    {{-- Start Site Setting  --}}
+        
+    <div id="sitesettings" class="sitesettings">
+        <div class="sitesettings-item"><a href="javascript:void(0);" id="sitetoggle" class="sitetoggle"><i class="fas fa-cog ani-rotates"></i></a></div>
+    </div>
+        
+
+    {{-- End Site Setting  --}}
+
+    {{-- Start Leftsidebar  --}}
+        @include('layouts.adminleftsidebar')
+    {{-- End Leftsidebar  --}}
+
+    {{-- Page Wrapper  --}}
+
+        <section>
+
+
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-10 col-md-9 pt-md-5 mt-md-3 ms-auto">
+
+                        {{-- start inner content area  --}}
+                      <div class="row">
+                        {{-- start breadcrumbs  --}}
+
+                        <nav>
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="javascript:void(0);"><i class="fas fa-home"></i></a></li>
+                                <li class="breadcrumb-item"><a href="javascript:void(0);">Previous</a></li>
+                                <li class="breadcrumb-item active"><a href="javascript:void(0);">Current</a></li>
+                            </ol>
+                        </nav>
+
+                        {{-- end breadcrumbs --}}
+
+                        @yield('content') 
+                      </div>
+
+                      {{-- end inner content area  --}}
+
+            
+
+                    </div>
+                </div>
+            </div>
+            
+        </section>
+
+    {{-- Page Wrapper  --}}
+
+</div>  
+{{-- end reactjs or vuejs  --}}      
+
+
+
+@include('layouts.adminfooter')
